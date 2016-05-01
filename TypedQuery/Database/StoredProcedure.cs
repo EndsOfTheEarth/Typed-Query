67efcd9301dadaf175239e83c65a23e7d249d658
jonathan <jonathan@JONATHAN-PC>
2016-05-01 16:13:26 +1200

    Updated copy right date

--- TypedQuery/Database/StoredProcedure.cs
+++ TypedQuery/Database/StoredProcedure.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * 
- * Copyright (C) 2009-2015 JFo.nz
+ * Copyright (C) 2009-2016 JFo.nz
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
